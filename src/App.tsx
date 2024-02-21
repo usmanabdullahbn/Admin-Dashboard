13628274f41aadff378818812a74fafeb2d9340b
Usman Abdullah <usmanabdullbn@gmail.com>
2024-02-21 21:15:46 +0500

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import Loader from "./components/Loader";
 import ProductManagement from "./pages/management/ProductManagement";
